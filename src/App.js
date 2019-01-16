b2ccddfe85ec64038c72847f93e5bcee277c2f6d
DESKTOP-DD\Dimitar <dimitar@megantic.net.au>
2019-01-16 09:12:39 +0100

    Task 1: input field (in App) with change listener which outputs the length of entered text below it in paragraph.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    inputText: ''
+  }
+
+  getInputText = (event) => {
+    let text = event.target.value;
+    this.setState({ inputText: text })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
+        <p>Input field</p>
+        <input onChange={this.getInputText} type='text' placeholder='type here'/>
+        <br/>
+        <p>Text length: {this.state.inputText.length}</p>
       </div>
     );
   }
